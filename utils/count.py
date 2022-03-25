6ff8951f87e44f13d59d7fef94fd317a213d922f
Robin-WZQ <60317828+Robin-WZQ@users.noreply.github.com>
2022-03-25 16:05:54 +0800

    Update count.py

--- utils/count.py
+++ utils/count.py
@@ -9,28 +9,19 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import json
 
-def seg_word(all) -> list:
-    '''
-    去除停用词
-    '''
-    stopwords = set()
-    with open('stopwords.txt','r',encoding='utf-8') as fr:
-        for i in fr:
-            stopwords.add(i.strip())
-    return list(filter(lambda x :x not in stopwords,all))
 
 def count_freq(words_list) -> dict:
     '''
     计算每个词出现频率
     '''
-    words_list_hash = {}
+    words_list_hash = {} # 定义字典，方便hash查找
     words_len = len(words_list)
     for i in words_list:
         if(len(i)>=1): #对长度大于等于1的字/词做统计
             if words_list_hash.get(i) == None: # 如果这个词未在字典中再统计
-                num = words_list.count(i)
-                frq = num/words_len
-                words_list_hash[i] = frq
+                num = words_list.count(i) # 统计出现个数
+                frq = num/words_len # 计算词频
+                words_list_hash[i] = frq # 字典赋值
     return words_list_hash
 
 def top_k(words_list_hash,k) -> dict:
@@ -39,20 +30,24 @@ def top_k(words_list_hash,k) -> dict:
     '''
     num = 0
     top_k_list = {}
+    # 以字典词频为关键字，从上到下排序
     a1 = sorted(words_list_hash.items(),key=lambda x:x[1],reverse=True)
     for i in a1:
         num+=1
         top_k_list[i[0]] = i[1]
-        if(num == k):
+        if(num == k): # 选取top-k个最高概率出现词语及对应词频
             break
     return top_k_list
 
 def one_hot(top_k_list) -> dict:
+    '''
+    one-hot 编码
+    '''
     data = []
     feature = {}
     for i in top_k_list:
         data.append(i)
-    values = array(data)
+    values = array(data) # 转换成numpy类型
 
     # 将其转换为数字（0~k-1）
     label_encoder = LabelEncoder()
@@ -63,6 +58,7 @@ def one_hot(top_k_list) -> dict:
     integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
     onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
 
+    # 转成字典记录
     for i in range(len(data)):
         feature[data[i]] = onehot_encoded[i]
 
@@ -76,22 +72,20 @@ def count_feature(file_name) -> array:
     all_words = []
 
     with open(file_name,'r',encoding='utf-8') as inp:
-        for line in inp.readlines(): #读入每行文字
-            line = line.rstrip("\n")
-            words = line.split(" ")
-            # words.pop()
-            
+        for line in inp.readlines(): # 读入每行文字
+            line = line.rstrip("\n") # 去掉最后的换行符
+            words = line.split(" ") # 以空格分割
+
             for word in words:
                 try:
-                    if word.split("/")[1] != 'O':
+                    if word.split("/")[1] != 'O': # 只统计标签为'BI'的，防止无关词涌入太多
                         all_words.append(word.split("/")[0])
                 except:
                     pass
 
     print("读入数据完毕")
-    all_words = seg_word(all_words)
     freq = count_freq(all_words)
-    final_freq = top_k(freq,k=500)
+    final_freq = top_k(freq,k=497)
 
     feature = one_hot(final_freq)
     return feature
@@ -116,7 +110,8 @@ if __name__ == "__main__":
     file_name = "./data/train/name_train.txt"
     feature = count_feature(file_name)
 
+    # 将最终one-hot保存到json文件中，方便日后读取
     operation = json.dumps(feature, cls=NumpyEncoder, ensure_ascii=False)
     f = open('count.json', 'w', encoding='utf-8')
     f.write(operation)
-    f.close()
+    f.close() # 注意用完及时关闭文件
