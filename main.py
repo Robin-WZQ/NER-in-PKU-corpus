e4f22af977f3c547d077875b98a6a3172eec55f6
Robin-WZQ <60317828+Robin-WZQ@users.noreply.github.com>
2022-03-25 16:05:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+'''
+@ file function: 主函数
+@ author: 王中琦
+@ data: 2022/3/17
+'''
+
 from datasets.Words_dataset import WordsDataset
 from train_val_test import train,testall
 from models.softmax import softmax_net
@@ -11,27 +17,15 @@ warnings.filterwarnings("ignore")
 
 
 def main():
+    # 自定义使用gpu or cpu
     device=torch.device ( "cuda:0" if torch.cuda.is_available () else "cpu")
 
+    # 导入数据
     train_dataset = WordsDataset(function="name",dataset="train")
     test_dataset = WordsDataset(function="name",dataset="test")
     val_dataset = WordsDataset(function="name",dataset="val")
 
-    net = softmax_net().to(device)
-
-    for name, param in net.named_parameters():
-        if 'weight' in name:
-            init.normal_(param, mean=0, std=0.01)
-        if 'bias' in name:
-            init.constant_(param, val=1)
-
-    optimizor= torch.optim.SGD(net.parameters(),lr=0.04,momentum=0.9)
-    criterion = torch.nn.CrossEntropyLoss()
-
-    n_epoches = 50
-    train_batch_size = 100
-    test_batch_size = 100
-    #建立一个数据迭代器
+    # 建立一个数据迭代器
     # 装载训练集
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                             batch_size=train_batch_size,
@@ -45,8 +39,27 @@ def main():
                                             batch_size=test_batch_size,
                                             shuffle=True)
 
+    # 导入模型至设备
+    net = softmax_net().to(device)
+
+    # 初始化模型参数
+    for name, param in net.named_parameters():
+        if 'weight' in name:
+            init.normal_(param, mean=0, std=0.01) # 权重使用正态分布
+        if 'bias' in name:
+            init.constant_(param, val=1) # 偏置使用常数
+
+    optimizor= torch.optim.SGD(net.parameters(),lr=0.04,momentum=0.9) # 使用SGD优化器
+    criterion = torch.nn.CrossEntropyLoss() # 交叉熵损失函数
+
+    n_epoches = 20 # 训练轮数
+    train_batch_size = 100 # 训练batch
+    test_batch_size = 100 # 测试batch
+
+    # 训练
     train(device,train_loader,optimizor,criterion,n_epoches,train_batch_size,net,val_loader)
 
+    # 测试
     testall(device,test_loader,net)
 
     print("Done!")
