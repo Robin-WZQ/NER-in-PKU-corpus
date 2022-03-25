4d1da564cda0925682e3eecb402aeb169f57e818
Robin-WZQ <60317828+Robin-WZQ@users.noreply.github.com>
2022-03-25 16:06:36 +0800

    Update wordvector.py

--- utils/wordvector.py
+++ utils/wordvector.py
@@ -15,7 +15,7 @@ def seg_word(all) -> list:
     stopwords = set()
     with open('stopwords.txt', 'r', encoding='utf-8') as fr:
         for i in fr:
-            stopwords.add(i.strip())
+            stopwords.add(i.strip()) # 删去所有停词
     return list(filter(lambda x: x not in stopwords, all))
 
 
@@ -29,8 +29,8 @@ def count_feature(file_name) -> array:
     with open(file_name, 'r', encoding='utf-8') as inp:
         for line in inp.readlines():  # 读入每行文字
             all_words = []
-            line = line.rstrip("\n")
-            words = line.split(" ")
+            line = line.rstrip("\n") # 去掉换行符
+            words = line.split(" ") # 以空格分割开
             words.pop()
 
             for word in words:
@@ -45,11 +45,11 @@ def count_feature(file_name) -> array:
 
 
 def model_train(token_list):
-    num_features = 500
-    min_word_count = 1 
-    num_workers = 1
-    window_size = 3
-    subsampling = 1e-3
+    num_features = 497 # 特征维度
+    min_word_count = 1  # 词语最小出现出现频率
+    num_workers = 1 # numworker读取器数量
+    window_size = 3 # 窗口大小为3
+    subsampling = 1e-3 
 
     model = Word2Vec(
         token_list,
@@ -58,12 +58,12 @@ def model_train(token_list):
         min_count=min_word_count,
         window=window_size,
         sample=subsampling,
-        epochs=100,
+        epochs=100, # 训练轮数
         sg=1
     )
 
     model.init_sims(replace=True)
-    model_name = "my_word2vec_skip"
+    model_name = "my_word2vec_skip" # 保存到该文件中
     model.save(model_name)
 
     return True
@@ -76,10 +76,11 @@ def main():
     if(model_train(token_list)):
         print("训练完成")
 
-    model = Word2Vec.load("my_word2vec_skip")
+    model = Word2Vec.load("my_word2vec_skip") # 读取word2vector模型
 
+    # 查看与目标词最相近（通过计算余弦相似度）的词语
     for e in model.wv.most_similar(positive=['泽民'], topn=10):
-        print(e[0], e[1])
+        print(e[0], e[1]) # 输出词语及其向量
 
 
 if __name__ == "__main__":
